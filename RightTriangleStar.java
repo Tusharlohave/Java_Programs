d43c8e7ff0808056371a7cd606648fd0b761b6a5
Tusharlohave <tusharlohawe140@gmial.com>
2024-01-17 18:38:59 +0530

    Print Right Triangle Star using java

---
+++ RightTriangleStar.java
@@ -0,0 +1,10 @@
+public class RightTriangleStar {
+    public static void main(String agrs[]){
+        for(int i=0;i<=4;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
